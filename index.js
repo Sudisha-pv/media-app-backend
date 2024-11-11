19cd60a3c4cd16c0bb8cfa6a6348884060da3157
Sudisha-pv <sudhishapv@gmail.com>
2024-11-11 10:05:44 +0530

    media app backend complited

---
+++ index.js
@@ -0,0 +1,26 @@
+//import json server
+const jsonServer = require('json-server')
+
+//2 create a server application using json server
+const mediaServer= jsonServer.create()
+
+//3 create a middleware used by json server
+const middleware = jsonServer.defaults()
+
+//4 set up route for db.json
+const router = jsonServer.router('db.json')
+
+//5 Applying middleware the server
+mediaServer.use(middleware)
+
+//6 Applying  router to the server
+mediaServer.use(router)
+
+//7 seting the port
+const PORT=3000
+
+//8 starting the server
+mediaServer.listen(PORT,()=>{
+ console.log("Media server started...listening on" +PORT);
+ 
+})
